fb8bbb49ecbc06ebe87dc5245b03b83a0630c6cf
sudarshan-sh <sudarshan2896@gmail.com>
2026-07-14 18:37:53 +0530

    default value shown in selected text

--- frontend-machine-coding-bank/app/problems/star-rating/page.tsx
+++ frontend-machine-coding-bank/app/problems/star-rating/page.tsx
@@ -3,7 +3,7 @@ import StarRating from "@/src/features/star-rating/components/StarRating";
 import { useState } from "react";
 
 export default function Page() {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(2); // default selected is 2
 
   const handleRating = (rating: number) => {
     console.log(rating);
@@ -12,7 +12,7 @@ export default function Page() {
 
   return (
     <main className="flex flex-col gap-3 min-h-screen items-center justify-center">
-      <StarRating defaultValue={2} onChange={handleRating} maxStars={5} />
+      <StarRating defaultValue={rating} onChange={handleRating} maxStars={5} />
       <p>Selected: {rating}</p>
     </main>
   );
