df21372859516473fdb529e58a1693db650bc363
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-04 17:12:12 +0530

    Remove duplicate Postgres folder, keep 02_PostgreSQL
    Both folders had identical content; consolidating to the numbered naming convention used by other topic folders.

--- Postgres/01_databases/03-list-databases.sql
+++
@@ -1,5 +0,0 @@
--- # to check all the DBs
-docker exec -it <container_name> psql -U <username> -d <database_name> -c "\l"
-ex- docker exec -it myDb psql -U sudarshan -d sdb -c "\l"
-
-SELECT datname FROM pg_database;
